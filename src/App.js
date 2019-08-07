0d3d8311f377bf2168d97d1d060c4674c774571d
Francis Gulotta <wizard@roborooter.com>
2019-08-07 11:30:27 -0400

    The create react app to use it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from 'react'
+import { Fade, Button } from '@material-ui/core'
+import { usePouch } from './usePouch'
+import { TodoInput } from './TodoInput'
+import './App.css'
 
 function App() {
+  console.log('render App')
+  const [todos, pouch] = usePouch('todos', { rejectErrors: true })
+  const [inputText, updateInput] = useState('')
+  const todosByDate = useMemo(() => Object.values(todos).reverse(), [todos])
+  const submit = () => {
+    if (inputText === '') {
+      return
+    }
+    pouch.put({ text: inputText })
+    updateInput('')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoInput value={inputText} updateValue={updateInput} onSubmit={submit} />
+      <div className="TodoList">
+        <h2>TODOS</h2>
+        {todosByDate.map((todo) => {
+          return (
+            <Fade in={true} key={todo._id}>
+              <div>
+                <code>{todo.text}</code>
+                <Button onClick={() => pouch.remove(todo)}>delete</Button>
+              </div>
+            </Fade>
+          )
+        })}
+      </div>
     </div>
   );
 }
