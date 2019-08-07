39df8f2585e5a7752e46f7e41200646201fd2b88
Francis Gulotta <wizard@roborooter.com>
2019-08-07 11:30:19 -0400

    A little multiline input component

---
+++ src/TodoInput.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import { TextField, InputAdornment, IconButton } from '@material-ui/core'
+import { AddCircleOutline } from '@material-ui/icons'
+
+export const TodoInput = ({ value, updateValue, onSubmit }) => {
+  const [multiline, setMultiline] = useState(false)
+  const submit = () => {
+    setMultiline(false)
+    onSubmit()
+  }
+
+  const inputChange = event => {
+    updateValue(event.target.value)
+  }
+  const onEnter = event => {
+    if (event.key !== 'Enter') {
+      return
+    }
+    const shift = event.getModifierState('Shift')
+    if (shift && !multiline) {
+      setMultiline(true)
+      return
+    }
+    if (!shift && multiline) {
+      return
+    }
+    event.preventDefault()
+    submit()
+  }
+
+  const inputProps= {
+    endAdornment: (
+      <InputAdornment position="end">
+        <IconButton
+          aria-label="save todo"
+          onClick={submit}
+        >
+          <AddCircleOutline />
+        </IconButton>
+      </InputAdornment>
+    )
+  }
+
+  return (
+    <>
+      <TextField
+      id="outlined-multiline-flexible"
+      label="What's on your mind?"
+      InputProps={inputProps}
+      multiline={true}
+      value={value}
+      onChange={inputChange}
+      margin="normal"
+      helperText={multiline ? "shit+enter for save" : "shit+enter for \\n"}
+      variant="outlined"
+      onKeyPress={onEnter}
+    />
+    </>
+  )
+}
